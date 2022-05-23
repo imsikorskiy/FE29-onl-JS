1022ceb65160d4e4634da3b440121a49c03b24d2
imsikorskiy <sikorskiy97@gmail.com>
2022-05-23 16:08:35 +0300

    added task-2

--- Vlad_Sikorskiy/homework-1/task-2/index.js
+++ Vlad_Sikorskiy/homework-1/task-2/index.js
@@ -1,1 +1,36 @@
 "use strict"
+
+let bulletNumber = 7;
+let shotsFired = 3;
+
+if (shotsFired <= 7 && bulletNumber <= 7 && shotsFired != 0 && bulletNumber != 0) {
+
+    let result = true;
+
+    for (let i = 1; i <= shotsFired; i++) {
+
+        if (bulletNumber === i) {
+
+            console.log('Бах!');
+
+            console.log('Player survived');
+
+            result = false;
+
+            break;
+        }
+
+        console.log('Щелк!');
+
+    }
+
+    if (result) {
+
+        console.log('Player survived');
+    
+    }
+    
+    else {
+    console.log('mistake :(');
+    }
+};    
