7bfaa3daca4e049cf28d19acb2b2f150824cfe94
imsikorskiy <sikorskiy97@gmail.com>
2022-05-23 13:57:54 +0300

    change code

--- Vlad_Sikorskiy/homework-1/index.js
+++ Vlad_Sikorskiy/homework-1/index.js
@@ -1,39 +1,31 @@
 "use strict"
 
 let a = prompt('enter a');
+
 let b = prompt('enter b');
+
 let c = prompt('enter c');
-const x = 2;
-let D =  b*b-4*a*c;
-{
-    if (a==0) 
-        alert "Mistake, a=0 not allowed!";
 
-    let D = b*b-4*a*c;
+const X = 2;
 
-    let x1, x2;
+const D = b**X - 4*a*c;
 
-    if (D>0) {
+if (D === 0) {
 
-        x1 = (-b+Math.sqrt(D)) / (2*a);
+let x = (-b +Math. sqrt(D) )/2*a;
 
-        x2 = (-b-Math.sqrt(D)) / (2*a);
+alert(x);
 
-         alert("Coefficient: a="+a+", b="+b+", c="+c+"\nTast solutions: X1=" +x1 +", X2=" + x2+"\nDiscriminant(D)=" + D);
+} else if (D > 0) {
 
-    }
+let x1 = (-b + Math.sqrt(D) ) /2*a;
 
-    else if(D==0) {
+let x2 = (-b - Math.sqrt(D) ) /2*a;
 
-        x1=-b/(2*a);
+alert(x1);
 
-        alert("Coefficient: a="+a+", b="+b+", c="+c+"\nTast solution: X=" +x1 +"\nDiscriminant(D)=" + D);
+alert(x2);
 
-    }
-    
-    else if(D<0) {
-
-        alert("Coefficient: a="+a+", b="+b+", c="+c+"\nNo solutions"\nDiscriminant(D)=" + D");
-
-    }
+} else {
+    alert('no answer')
 };
